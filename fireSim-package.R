c57e0378e553580ef675193973c615499b540b05
Rainer M. Krug <R.M.Krug@gmail.com>
2013-04-25 13:32:39 +0200

    correct .gitignore

--- fireSim-package.R
+++ fireSim-package.R
@@ -4,7 +4,7 @@
 ##' Package: \tab fireSim\cr
 ##' Type: \tab Package\cr
 ##' Version: \tab 0.0.1\cr
-##' Date: \tab 2013-04-25_13-12\cr
+##' Date: \tab 2013-04-25_13-31\cr
 ##' License: \tab GPL (>= 2)\cr
 ##' LazyLoad: \tab yes\cr
 ##' }
