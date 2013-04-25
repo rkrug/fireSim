c35ab85cb5a6ad20fc0e2dee23b8aa3dfc9acb19
Rainer M. Krug <R.M.Krug@gmail.com>
2013-04-25 13:30:54 +0200

    Initial commit

---
+++ fireSim-package.R
@@ -0,0 +1,14 @@
+##' Simulate fire using different approaches
+##' 
+##' \tabular{ll}{
+##' Package: \tab fireSim\cr
+##' Type: \tab Package\cr
+##' Version: \tab 0.0.1\cr
+##' Date: \tab 2013-04-25_13-12\cr
+##' License: \tab GPL (>= 2)\cr
+##' LazyLoad: \tab yes\cr
+##' }
+##'
+##' @docType package
+##' @author Rainer M Krug \email{Rainer@@krugs.de}
+NULL
