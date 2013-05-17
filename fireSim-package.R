a2998137e0f962093e20d3408c6d304bb60eefeb
Rainer M. Krug <R.M.Krug@gmail.com>
2013-05-17 19:00:50 +0200

    Minor changes

--- fireSim-package.R
+++ fireSim-package.R
@@ -4,7 +4,7 @@
 ##' Package: \tab fireSim\cr
 ##' Type: \tab Package\cr
 ##' Version: \tab 0.0.1\cr
-##' Date: \tab 2013-04-25_13-31\cr
+##' Date: \tab 2013-05-16_15-07\cr
 ##' License: \tab GPL (>= 2)\cr
 ##' LazyLoad: \tab yes\cr
 ##' }
